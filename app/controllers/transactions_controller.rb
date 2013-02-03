eca76d6ebce99f2e49d9206e493780ab4a6c3594
Jeffrey Ravetto <jravetto@hotmail.com>
2013-02-03 12:41:17 -0500

    added functionality to change years in the register

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,8 +13,9 @@ def index
 		if params[:date]
 			# switch to use a new month
 			m = params[:date][:month]
+			y = params[:date][:year]
 			session[:active_month] = m
-			session[:active_year]=Date.today.year
+			session[:active_year]=y
 			logger.info 'switching to month # ' + m.to_s
 		end
 	
@@ -22,13 +23,13 @@ def index
 		@year=session[:active_year]
 		
 		## just for now...
-		@year=2012
+		## @year=2012
 		
 		logger.info 'using month # ' + @month.to_s
 		logger.info 'using year  # ' + @year.to_s
 		
 		# create a listing of all the transactions
-		@start_date = Date.new(@year, @month.to_i, 1)
+		@start_date = Date.new(@year.to_i, @month.to_i, 1)
 		@end_date = @start_date.end_of_month
 	
 		
